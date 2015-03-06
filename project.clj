7f5bae9ce2ca22002ac6b01976b74616d6603402
Felix Andrews <felix@nfrac.org>
2015-03-06 10:33:44 +0800

    initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject org.nfrac/motility "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.nfrac/gpai "0.1.0-SNAPSHOT"]
+                 [org.nfrac/bok.cljplayer "0.1.0-SNAPSHOT"]
+                 [org.clojure/clojure "1.6.0"]])
